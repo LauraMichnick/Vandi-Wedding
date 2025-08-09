d415b36e76fa4741bac79c72c4e49ea12fd57d88
LauraMichnick <michnick.laura@gmail.com>
2025-08-09 18:16:38 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+ /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function toggleMenu(x) {
+    x.classList.toggle("change");
+    document.getElementById("menu").classList.toggle("show");
+  } 
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.closest('.menubtn')) {
+        const bar2 = document.querySelector('.bar2');
+        if (bar2) {
+          const opacity = window.getComputedStyle(bar2).getPropertyValue('opacity');
+          if (opacity === '0') {
+            document.getElementById("menubtn").classList.toggle("change");
+          }
+        }
+      var dropdowns = document.getElementsByClassName("menu-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+
+    }
+  }
